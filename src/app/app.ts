69aa3d4dde5e9dfd1b6040c3386ac66cd384215d
Jacques <jacquesvanluyck@gmail.com>
2026-01-21 08:50:18 +0100

    generated app header component, fixes #1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Filter } from './filter/filter';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [Filter],
+  imports: [Filter, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
