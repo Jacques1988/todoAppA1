45bf6cba0b1c9ad53a4b3574ab4d123fa18ce25e
Jacques <jacquesvanluyck@gmail.com>
2026-01-26 09:07:38 +0100

    generated component, build responsive-design, fixes #12, fixes #13, fixes #14, fixes #15

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Filter } from './filter/filter';
 import { Header } from './header/header';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-root',
-  imports: [Filter, Header],
+  imports: [Filter, Header, NewTask],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
